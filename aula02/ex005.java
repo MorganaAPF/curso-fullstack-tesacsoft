7ac247a53221871932ac77549a366b276a77af3e
MorganaAPF <morganaapf@gmail.com>
2026-02-27 15:31:41 +0100

    aula04 first commit

---
+++ aula02/ex005.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class ex005 {
+    public static void main(Strings[] args) {
+        Scaner leitor = new Scanner(System.in);
+        double nota1, nota2, media;
+        System.out.print("Digite a primeira nota: ");
+        nota1 = leitor.nextDouble();
+        System.out.print("Digite a segunda nota: ");
+        System.out.println("Média = " + (nota1 + nota 2 / 2);
+    }
+}
